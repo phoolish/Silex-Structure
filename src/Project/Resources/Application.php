d08607f02b7b79c29d3d296c00cc32010e3c9c91
ChrisRiddell <chris@codeject.com>
2013-03-03 09:19:05 +1030

    Changed to chaining and added some phpdoc

--- src/Project/Resources/Application.php
+++ src/Project/Resources/Application.php
@@ -8,40 +8,79 @@ use Silex\Provider\UrlGeneratorServiceProvider;
 
 class Application extends BaseApplication
 {
+    /**
+     * Constructor
+     *
+     * @return void
+     */
     public function __construct()
     {
         parent::__construct();
 
-        $this->registerParameters();
-        $this->registerServiceProviders();
-        $this->registerControllers();
-        $this->registerModels();
-        $this->registerRoutes();
+        $this->registerParameters()
+             ->registerServiceProviders()
+             ->registerControllers()
+             ->registerModels()
+             ->registerRoutes();
     }
 
+    /**
+     * Register parameters
+     *
+     * @return Application
+     */
     private function registerParameters()
     {
         $this['debug'] = true;
+
+        return $this;
     }
 
+    /**
+     * Register silex service providers
+     *
+     * @return Application
+     */
     private function registerServiceProviders()
     {
         $this->register(new ServiceControllerServiceProvider());
         $this->register(new UrlGeneratorServiceProvider());
+
+        return $this;
     }
 
+    /**
+     * Register controller classes to the Silex/Pimple DIC when using
+     * ServiceControllerServiceProvider
+     *
+     * @return Application
+     */
     private function registerControllers()
     {
         // Fix for 5.3 (still needed with php5.4 as silex does not support the rebind)
         $app = $this;
+
+        return $this;
     }
 
+    /**
+     * Register model classes to the Silex/Pimple DIC
+     *
+     * @return Application
+     */
     private function registerModels()
     {
         // Fix for 5.3 (still needed with php5.4 as silex does not support the rebind)
         $app = $this;
+
+        return $this;
     }
 
+    /**
+     * Register silex routes
+     *
+     * @return Application
+     */
     private function registerRoutes()
     {
         // Fix for 5.3 (still needed with php5.4 as silex does not support the rebind)
@@ -51,5 +90,7 @@ class Application extends BaseApplication
         $this->get('/', function() use ($app) {
             return 'Hello World';
         });
+
+        return $this;
     }
 }
