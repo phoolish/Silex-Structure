6866111a4f98489752efcdab82d818b13cff36f4
ChrisRiddell <chris@codeject.com>
2013-07-15 18:21:30 +0930

    fix composer autoload path

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
 use Symfony\Component\Stopwatch\Stopwatch;
 use Project\Resources\Application;
 
-require_once __DIR__.'../vendor/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 $stopwatch = new Stopwatch();
 $stopwatch->start('bench');
@@ -12,4 +12,4 @@ $app = new Application();
 $app->run();
 
 $stopwatch = $stopwatch->stop('bench');
-echo sprintf("<!--Time: %s Seconds, Memory: %s Megabyte-->", $stopwatch->getDuration() / 1000, ($stopwatch->getMemory() / 1024) / 1024 );
+echo sprintf("<!--Time: %s Seconds, Memory: %s Megabyte(s)-->", $stopwatch->getDuration() / 1000, ($stopwatch->getMemory() / 1024) / 1024 );
